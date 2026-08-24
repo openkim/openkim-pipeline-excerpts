86402cabb0939755bfaf99497f39bd810e847e8f
Claire Waters <bwaters@umn.edu>
2026-08-24 11:26:04 -0500

    MOD: add check_units_util and make __init__.py explicit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from kimunits import *
+from kimunits import __all__ as kimunits_all
+
+__all__ = kimunits_all
--- kimunits.py
+++ kimunits.py
@@ -11,7 +11,8 @@ import re
 import math
 import subprocess
 import warnings
-import sys
+import numpy as np
+
 # sys.path.append("..")
 # from logger import logging
 warnings.simplefilter("ignore")
@@ -19,10 +20,44 @@ warnings.simplefilter("ignore")
 # logger = logging.getLogger("pipeline").getChild("kimunits")
 # logger.setLevel(logging.DEBUG)
 
+__all__ = ["UnitConversion",
+           "check_units_util",
+           "linear_fit",
+           "islinear",
+           "convert_units",
+           "convert_list",
+           "add_si_units",]
+
 class UnitConversion(Exception):
     """ Class for unit conversion errors """
 
 _units_output_expression = re.compile("(?P<value>(?:[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?))(?: (?P<unit>.+))?")
+
+def check_units_util() -> str:
+    """
+    Figure out if units (first choice) or gunits (second choice) works
+    with the options that we use
+    """
+    args = ["-o", r"%1.15e", "-qt1", "0.0 eV/angstrom^3 bar"]
+    try:
+        output = subprocess.check_output(["units"] + args, encoding="utf-8")
+        assert np.isclose(float(output), 0)
+        units_command = "units"
+    except Exception:
+        try:
+            output = subprocess.check_output(["gunits"] + args, encoding="utf-8")
+            assert np.isclose(float(output), 0)
+            units_command = "gunits"
+        except Exception:
+            raise UnitConversion(
+                "Neither "
+                r"units -o %1.15e -qt1 '0.0 eV/angstrom^3 bar'"
+                " nor "
+                r"gunits -o %1.15e -qt1 '0.0 eV/angstrom^3 bar'"
+                " successfully ran and returned 0.e0. Please install a "
+                "compatible version of units."
+            )
+    return units_command
 
 def linear_fit(x, y):
     """
@@ -53,6 +88,7 @@ def islinear(unit, to_unit=None):
 
 def convert_units(from_value, from_unit, wanted_unit=None, suppress_unit=False):
     """ Works with 'units' utility """
+    units_util = check_units_util()
     from_sign = from_value < 0
     from_value = str(abs(from_value))
     from_unit = str(from_unit)
@@ -60,10 +96,10 @@ def convert_units(from_value, from_unit, wanted_unit=None, suppress_unit=False):
     TEMPERATURE_FUNCTION_UNITS=["degC","tempC","degF","tempF"]
 
     if from_unit in TEMPERATURE_FUNCTION_UNITS :
-        args = ['units','-o', '%1.15e', '-qt1', ''.join((from_unit,'(',from_value,')'))]
+        args = ['units_util','-o', '%1.15e', '-qt1', ''.join((from_unit,'(',from_value,')'))]
 
     else :
-        args = ['units','-o', '%1.15e', '-qt1', ' '.join((from_value, from_unit))]
+        args = ['units_util','-o', '%1.15e', '-qt1', ' '.join((from_value, from_unit))]
 
     if wanted_unit:
         args.append(wanted_unit)
